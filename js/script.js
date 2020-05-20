3bdf45a990f86d986e87057677e95050d9432fe7
cleophas <makokhacleophas@gmail.com>
2020-05-21 01:08:33 +0300

    adda function to show text when clicked

--- js/script.js
+++ js/script.js
@@ -10,9 +10,27 @@ $(function () {
     $(devp).hide();
     $(prop).hide();
 
+    //Show paragraph content
+        $('.des').on('click', function () {
+            $('.des').slideUp('slow');
+            $(desp).show(500);
+            $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+
+        });
+        $('.dev').on('click', function () {
+            $('.dev').slideUp('slow');
+            $(devp).show(500);
+            $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+        });
+        $('.pro').on('click', function () {
+            $('.pro').slideUp('slow');
+            $(prop).show(500);
+            $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
+        });
 $("button.findMatch").click(function(){
     let age = $(".ageGroup option:selected").val();
     //add array for age groups
     alert("hey");
-    
+
+
 });
