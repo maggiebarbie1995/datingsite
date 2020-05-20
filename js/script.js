b0bc3945badbc5c19339befb7848b128163ad245
cleophas <makokhacleophas@gmail.com>
2020-05-21 01:06:31 +0300

    Initiates the js function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
+
 $("button.findMatch").click(function(){
     let age = $(".ageGroup option:selected").val();
     //add array for age groups
