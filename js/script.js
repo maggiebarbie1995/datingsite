8ca604a9b58696afbf4128b77f999dc934ff76f9
ramzan <ramzanthenolph@gmail.com>
2020-05-21 12:37:26 +0300

    add input fields to be required

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
-    $("button#findMatch").click(function(){
+    $("button#findMatch").submit(function(){
+        event.preventDefault();
+
+        $("input").attr("required", "true");
+        $("select").attr("required", "true");
         let age = $(".ageGroup option:selected").val();
         var phoneNumber = $(this).find("input#phone").val();
-        alert("Your request has been received. Processing your match");
+        alert("Your request has been received. Processing your match");        
+        
         
         
     });
