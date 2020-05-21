bb605c11b41813c5f72c7087e5bfdaa7149b3504
ramzan <ramzanthenolph@gmail.com>
2020-05-21 12:16:06 +0300

    add alert notification

--- js/script.js
+++ js/script.js
@@ -1,21 +1,9 @@
 $(document).ready(function(){
     $("button#findMatch").click(function(){
         let age = $(".ageGroup option:selected").val();
-        let username = $()
-        //add array for age groups
-
-        if(username == " "){
-            console.log("Please enter username");
-        }
-        if(password != password){
-            console.log("Incorrect password. Please try again");
-        }
-        switch(age){
-            case age= "0":
-                console.log("Please select age group")
-            break;
-            case age= "1":
-        }
+        var phoneNumber = $(this).find("input#phone").val();
+        alert("Your request has been received. Processing your match");
+        
         
     });
 });
