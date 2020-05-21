c8862806845ff83dcf75c75f27d896b1c77f25ba
ramzan <ramzanthenolph@gmail.com>
2020-05-21 09:34:23 +0300

    add username and password if statements

--- js/script.js
+++ js/script.js
@@ -1,6 +1,21 @@
-$("button.findMatch").click(function(){
-    let age = $(".ageGroup option:selected").val();
-    //add array for age groups
-    alert("hey");
-    
+$(document).ready(function(){
+    $("button#findMatch").click(function(){
+        let age = $(".ageGroup option:selected").val();
+        let username = $()
+        //add array for age groups
+
+        if(username == " "){
+            console.log("Please enter username");
+        }
+        if(password != password){
+            console.log("Incorrect password. Please try again");
+        }
+        switch(age){
+            case age= "0":
+                console.log("Please select age group")
+            break;
+            case age= "1":
+        }
+        
+    });
 });
